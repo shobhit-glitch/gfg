9349f9bea6619d53f85ef1ace317dd5ed23c62fd
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-06-21 02:14:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Coding/reverse-coding.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        
+        long long m = 1000000007;
+
+        long long sum = (n% m * (n+1)% m)/2;
+
+        return sum % m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
