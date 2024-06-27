1fb03fd7cc996f7b7c43429bdf42992b2a4c45bc
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-27 17:39:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max sum in sub-arrays/max-sum-in-subarrays.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+        long long pairWithMaxSum(long long arr[], long long N)
+    {
+        // Your code goes here
+        long long maxs=0; //arr[N-2]+arr[N-1];
+        for(int i=1;i<N;i++){
+            maxs=max(maxs,arr[i]+arr[i-1]);
+        }
+        
+        return maxs;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll a[n];
+	    for(ll i=0;i<n;i++)
+		    cin>>a[i];
+		Solution ob;
+	    cout<<ob.pairWithMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
