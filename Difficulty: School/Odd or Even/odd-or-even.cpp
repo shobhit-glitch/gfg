bb24f761917729f45216349e5861f324665981cc
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-07-06 12:26:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Odd or Even/odd-or-even.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string oddEven(int N){
+        // code here 
+        int bit=N&1;
+        bool b=bit&1;
+        if(b){
+            return "odd";
+        }
+        return "even";
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.oddEven(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
