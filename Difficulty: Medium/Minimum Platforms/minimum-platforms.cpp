a80a0cfabc7c7160c55aa88c84b83558b3bf216a
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-07-20 04:36:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	int mp=0;
+    	for(int i=0;i<2400;i++){
+    	    int cnt=0;
+    	    for(int j=0;j<n;j++){
+    	        if(i>=arr[j] && i<=dep[j]){
+    	            cnt++;
+    	        }
+    	        mp=max(mp,cnt);
+    	    }
+    	}
+    	return mp;
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
